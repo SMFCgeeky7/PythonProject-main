1ada9eb565e8e1c7b4839ea21087573d2de06920
Sebastian Fernandez <sebastian_fernandez_conde@hotmail.com>
2025-06-16 21:05:21 -0400

    Cambios en el user_service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,19 @@
-from app.models.usuario import User
+from app.models.User import User
 from app.services.base_service import BaseService
+from app import db
 
-class UserService(BaseService):
+class NoteService(BaseService):
+    """Servicio para operaciones específicas de User"""
+
     def __init__(self):
         super().__init__(User)
+
+    @staticmethod
+    def get_all():
+        """Obtener todas las notas"""
+        return Note.query.all()
+
+    @staticmethod
+    def get_by_id(user_id):
+        """Obtener nota por ID"""
+        return Note.query.get(user_id)
